784d5ad9c4d0ecdff264ae20d04382aba67104a7
luoChen <hank.luo@xianghuanji.com>
2021-09-10 15:55:56 +0800

    添加实现

--- flutter_bridge/android/src/main/kotlin/com/niluogeg/flutterbridge/flutter_bridge/FlutterBridgePlugin.kt
+++ flutter_bridge/android/src/main/kotlin/com/niluogeg/flutterbridge/flutter_bridge/FlutterBridgePlugin.kt
@@ -11,12 +11,11 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** FlutterBridgePlugin */
-class FlutterBridgePlugin: FlutterPlugin, MethodCallHandler, Message.NativeRouterApi {
+class FlutterBridgePlugin: FlutterPlugin, Message.NativeRouterApi {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     Message.NativeRouterApi.setup(flutterPluginBinding.binaryMessenger,this)
