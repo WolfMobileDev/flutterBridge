060e19bd0ca256ea46f31e7acb5e523e12b385b1
luoChen <hank.luo@xianghuanji.com>
2021-09-10 11:09:49 +0800

    添加 pigeon

---
+++ flutter_bridge/pigeon/messages.dart
@@ -0,0 +1,44 @@
+import 'dart:collection';
+
+import 'package:pigeon/pigeon.dart';
+
+class CommonParams {
+  String pageName;
+  String uniqueId;
+  Map<String, Object> arguments;
+  bool opaque;
+  String key;
+}
+
+class EventParam{
+
+}
+
+class StackInfo {
+  List<String> containers;
+  Map<String, List<Map<String, Object>>> routes;
+}
+
+@HostApi()
+abstract class NativeRouterApi {
+  void pushNativeRoute(CommonParams param);
+  void saveStackToHost(StackInfo stack);
+}
+
+@FlutterApi()
+abstract class FlutterRouterApi {
+  void pushRoute(CommonParams param);
+  void popRoute(CommonParams param);
+}
+
+
+
+//D:\soft\flutter\flutter_2.0.3\flutter\bin\flutter pub run pigeon --input pigeon/messages.dart
+void configurePigeon(PigeonOptions opts) {
+  opts.dartOut = 'lib/messages.dart';
+  opts.objcHeaderOut = 'ios/Classes/messages.h';
+  opts.objcSourceOut = 'ios/Classes/messages.m';
+  opts.objcOptions.prefix = 'FB';
+  opts.javaOut =
+      'android/src/main/kotlin/com/niluogeg/flutterbridge/flutter_bridge/Message.java';
+}
