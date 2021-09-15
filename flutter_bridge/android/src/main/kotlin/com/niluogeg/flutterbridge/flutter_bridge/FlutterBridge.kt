a7ef67c0093356284cb04852b2e3369710eb6274
luoChen <hank.luo@xianghuanji.com>
2021-09-15 18:42:48 +0800

    原生

--- flutter_bridge/android/src/main/kotlin/com/niluogeg/flutterbridge/flutter_bridge/FlutterBridge.kt
+++ flutter_bridge/android/src/main/kotlin/com/niluogeg/flutterbridge/flutter_bridge/FlutterBridge.kt
@@ -17,6 +17,7 @@ class FlutterBridge private constructor() : MethodChannel.MethodCallHandler {
     companion object {
         val instance: FlutterBridge by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) { FlutterBridge() }
         const val ERROR_CODE_ILLEGAL_METHODHANDLER = "0001"
+        const val ERROR_CODE_ERROR = "0002" // 调用报错
     }
 
 
@@ -27,30 +28,47 @@ class FlutterBridge private constructor() : MethodChannel.MethodCallHandler {
         methodMap[methodName] = methodHandler
     }
 
+    /**
+     * 反 注册方法
+     */
+    fun unRegisterHandler(methodName: String): MethodHandler? {
+        return methodMap.remove(methodName)
+    }
+
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-        Log.e("FlutterBridge", "call=${call.method} arguments=${call.arguments}")
-        val methodName = call.method ?: ""
-        val params = if (call.arguments == null) HashMap() else call.arguments as Map<String, Any?>
-        val methodHandle = methodMap[methodName]
-        if (methodHandle != null) {
-            when (methodHandle) {
-                is MethodHandlerHaveReturn -> {
-                    result.success(methodHandle.onMethodCall(params))
+        try {
+            Log.e("FlutterBridge", "call=${call.method} arguments=${call.arguments}")
+            val methodName = call.method ?: ""
+            val params =
+                if (call.arguments == null) HashMap()
+                else call.arguments as Map<String, Any?>
+            val methodHandle = methodMap[methodName]
+            if (methodHandle != null) {
+                when (methodHandle) {
+                    is MethodHandlerHaveReturn -> {
+                        result.success(methodHandle.onMethodCall(params))
+                    }
+                    is MethodHandlerNoReturn -> {
+                        methodHandle.onMethodCall(params)
+                        result.success("No Return Method Handler")
+                    }
+                    is MethodHandlerHaveReturnAsync -> {
+                        methodHandle.onMethodCall(params, MethodResult(result))
+                    }
+                    else -> {
+                        result.error(
+                            ERROR_CODE_ILLEGAL_METHODHANDLER,
+                            "illegal MethodHandler",
+                            null
+                        )
+                    }
                 }
-                is MethodHandlerNoReturn -> {
-                    methodHandle.onMethodCall(params)
-                    result.success("No Return Method Handler")
-                }
-                is MethodHandlerHaveReturnAsync -> {
-                    methodHandle.onMethodCall(params, MethodResult(result))
-                }
-                else -> {
-                    result.error(ERROR_CODE_ILLEGAL_METHODHANDLER, "illegal MethodHandler", null)
-                }
+            } else {
+                result.notImplemented()
             }
-        } else {
-            result.notImplemented()
+        } catch (e: Exception) {
+            result.error(ERROR_CODE_ERROR, e.message, e)
         }
     }
 
